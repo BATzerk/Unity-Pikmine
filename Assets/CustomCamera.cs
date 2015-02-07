d00d945738f0ba0b9e3a1b2f567849d02a5699a8
BATzerk <batzerk@gmail.com>
2015-02-07 13:06:20 -0500

    Added simple character and camera controller

---
+++ Assets/CustomCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomCamera : MonoBehaviour {
+	// Settables
+	private float distanceUp = 3f;
+	private float distanceAway = 5f;
+	private float positionEasing = 2f;
+	// References
+	[SerializeField]
+	Transform myTarget;
+	// Properties
+	private Vector3 targetPosition; // where I aim to put myself (we ease into this position)
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		// Move to a nice position behind my target!
+		targetPosition = myTarget.position + Vector3.up*distanceUp - myTarget.forward*distanceAway;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime*positionEasing);
+		// Look at my target!
+		transform.LookAt (myTarget);
+	}
+}
